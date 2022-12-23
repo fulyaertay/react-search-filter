56e1eb3a6a91b14c038eff567798810e2913b6b4
Fulya <fulya.ertay@gmail.com>
2022-12-24 00:51:14 +0300

    feat: add useState to filter

--- src/App.js
+++ src/App.js
@@ -3,8 +3,38 @@ import Data from './data.json';
 import './App.css';
 
 function App() {
+  const[searchValue,setSearchValue]=useState("");
+
   return (
-    <div className="App">
+    <div className="container">
+      <h2>Urla Gezilecek Yerler</h2>
+      <div className='inputContainer'>
+        <input className='search' type="text" placeholder='Gezilecek Yer Ara..' onChange={(e)=>setSearchValue(e.target.value)}>
+
+        </input>
+      </div>
+      <div className='dataContainer'>
+        {
+          Data
+          .filter((val)=>{
+            if(searchValue==""){
+              return val;
+            }else if(val.title.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase())){
+              return val;
+            }
+          }).map((val)=>{
+            return (
+              <div className="data" key={val.id}> 
+                <img src={val.image}></img>
+                <h3>{val.title}</h3>
+                <p>{val.description}</p>
+              </div>
+            )
+
+          })
+        }
+
+      </div>
      
     </div>
   );
